8e7881cfd53d62305d24d598730e032773e24f95
Mark Garcia <mymgcode@gmail.com>
2018-10-18 14:13:46 -0500

    - tunered result tables into a directive - added result boxes - added a modal - added a loading - added a scroll to results animation

--- assets/js/foursquare_simple.js
+++ assets/js/foursquare_simple.js
@@ -1,9 +1,112 @@
 var myapp = angular.module('myapp', []);
 
 
+myapp.directive("modal", function() {
+	return {
+		template :
+`
+<div>
+	<div class="modal fade" role="dialog" tabindex="-1" id="modal1">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">{{details.name}}</h4><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+				<div class="modal-body">
+					<img ng-if="details.bestPhoto.prefix" src="{{details.bestPhoto.prefix}}300x500{{details.bestPhoto.suffix}}">
+					<div ng-if="!details.bestPhoto.prefix">Sorry, no info found. <a ng-href="{{details.canonicalUrl}}" target="_blank">{{details.canonicalUrl}}</a></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+`
+	};
+});
+
+
+myapp.directive("resultsTable", function() {
+	return {
+		template : 
+`
+<table class="table results">
+	<thead>
+		<tr>
+			<th>#</th>
+			<th ng-if="has_distance" ng-click="tableSort('Distance')" >
+				<span class="sort">
+					Distance 
+					<span ng-if="orderByField == 'location.distance'">
+						<span ng-show="reverseSort"><i class="fa fa-sort-numeric-asc"></i></span>
+						<span ng-show="!reverseSort"><i class="fa fa-sort-numeric-desc"></i></span>
+					</span>
+				</span>
+			</th>
+			<th ng-click="tableSort('Name')">
+				<span class="sort">
+					Place 
+					<span ng-if="orderByField == 'name'">
+						<span ng-show="reverseSort"><i class="fa fa-sort-alpha-desc"></i></span>
+						<span ng-show="!reverseSort"><i class="fa fa-sort-alpha-asc"></i></span>
+					</span>
+				</span>
+			</th>
+			<th>Share</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr ng-repeat="result in results | orderBy :orderByField:reverseSort">
+			<th scope="row" data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-duration="1000">{{$index+1}}</th>
+			<td ng-if="has_distance" data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-duration="30000">{{result.location.distance}} meters</td>
+			<td data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-duration="30000" class="place"><a href="{{result.link}}">{{result.name}}</a></td>
+			<td data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-duration="30000" class="social">
+				<a target="_blank" ng-href="https://www.facebook.com/sharer/sharer.php?u={{result.link}}"><i class="fa fa-facebook"></i></a>
+				<a target="_blank" ng-href="https://twitter.com/home?status={{result.link}}"><i class="fa fa-twitter"></i></a>
+				<a target="_blank" ng-href="https://plus.google.com/share?url={{result.link}}"><i class="fa fa-google-plus"></i></a>
+				<span class="side-tooltip" ng-click="copyLnk(result.link,$event)"><i class="fa fa-link"></i></span>
+			</td>
+		</tr>
+	</tbody>
+</table>
+`
+	};
+});
+
+myapp.directive("resultsBoxes", function() {
+	return {
+		template : 
+`
+<div class="row justify-content-center results">
+	<div class="col-sm-6 col-lg-4" ng-repeat="result in results | orderBy :orderByField:reverseSort">
+		<div class="card clean-card text-center" data-aos="zoom-in-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
+			<div class="card-body info">
+				<h4 class="card-title"><a href="#" data-toggle="modal" data-target="#modal1" ng-click="getDetails(result.id)">{{result.name}}</a> | <a href="{{result.link}}" target="_blank"><i class="fa fa-external-link"></i></a></h4>
+				<p class="card-text" ng-if="result.categories[0].name">{{result.categories[0].name}}</p>
+				<p class="card-text" ng-if="!result.categories[0].name">Uncategorized</p>
+				<p class="card-text">{{result.location.distance}} meters <a target="_blank" href="http://maps.google.com/maps?q={{result.location.lat}},{{result.location.lng}}"><i class="fa fa-map"></i></a></p>
+				<div class="social">
+					<a target="_blank" ng-href="https://www.facebook.com/sharer/sharer.php?u={{result.link}}"><i class="fa fa-facebook"></i></a>
+					<a target="_blank" ng-href="https://twitter.com/home?status={{result.link}}"><i class="fa fa-twitter"></i></a>
+					<a target="_blank" ng-href="https://plus.google.com/share?url={{result.link}}"><i class="fa fa-google-plus"></i></a>
+					<span class="side-tooltip" ng-click="copyLnk(result.link,$event)"><i class="fa fa-link"></i></span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+`
+	};
+});
+
 myapp.directive("showResults", function() {
 	return {
-		template : "<caption>Search for <i>( {{search_val}} ) </i> <b>{{results.length}}</b> Search results</caption>"
+		template : 
+`
+<span ng-show="searched">
+	<span ng-hide="no_result">
+		<caption>Search for <i>( {{search_val}} ) </i> <b>{{results.length}}</b> Search results</caption>
+	</span>
+</span>
+`
 	};
 });
 
@@ -16,22 +119,46 @@ myapp.directive("alertBox", function() {
 </div>`
 	};
 });
+
+myapp.directive("loading", function() {
+	return {
+		template :
+`<div id="CSSloaderOverlay">
+	<div id="CSSloader"></div>
+</div>
+`
+	};
+});
+
+// function that is trigger on the enter key
 myapp.directive('myEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.myEnter);
-                });
-                event.preventDefault();
-            }
-        });
-    };
+	return function (scope, element, attrs) {
+		element.bind("keydown keypress", function (event) {
+			if(event.which === 13) {
+				scope.$apply(function (){
+					scope.$eval(attrs.myEnter);
+				});
+				event.preventDefault();
+			}
+		});
+	};
 });
 myapp.controller("myCtrl", function($scope, $http) {
 	$scope.alert_msg = "There has been an error.";
 	$scope.no_result = true;
 	$scope.searched = false;
+
+
+	$scope.getDetails = function(id="") {
+		$http.get("https://api.foursquare.com/v2/venues/"+id+"?client_id=UCOSSBXHSTZ3UIAA4RKO5DPXCVJQEOG1NINJ1LDUNL0LEJ4F&client_secret=Q3CGAJFLKGOELNT3I3YPUAQKEHMI20CN4J2QDZGBHD433QDS&v=20180323").then(
+			function(r) {
+				console.log(r.data.response.venue.bestPhoto);
+				$scope.details = r.data.response.venue;
+			}, function() {})
+
+	}
+
+
 	$scope.msg = function(msg) {
 		$scope.notification_msg = msg;
 		$("#snackbar").addClass("show");
@@ -81,6 +208,8 @@ myapp.controller("myCtrl", function($scope, $http) {
 
 	// this is to get search results, it is being called at the time after html5 geo api
 	$scope.getSearch = function(lat="", lng="") {
+		// sets up the loading to let the user know its proccessing
+		document.getElementById("CSSloaderOverlay").style.display = "block";
 		var appendLocation = "";
 		if ($scope.nearlocation == "") {
 			appendLocation = "&ll="+lat+","+lng;
@@ -93,14 +222,25 @@ myapp.controller("myCtrl", function($scope, $http) {
 					r.data.response.venues[i].link = "https://foursquare.com/v/"+r.data.response.venues[i].name.replace(/ /g, '-')+"/"+r.data.response.venues[i].id;
 					if(r.data.response.venues[i].location.distance!=undefined){
 						$scope.has_distance = true;
-					}
+					}	
 				}
 				$scope.results = r.data.response.venues;
 				$scope.no_result = false;
+				// hides the loading
+				document.getElementById("CSSloaderOverlay").style.display = "none";
+				$scope.msg(r.data.response.venues.length+" results found");
 				// the timeout is for to reset the AOS animation, because it is not detecting the new length of the page
-				setTimeout(function() {AOS.init({ disable: 'mobile' });}, 1000);
+				setTimeout(function() {
+					AOS.init({ disable: 'mobile' });
+					$([document.documentElement, document.body]).animate({
+						scrollTop: $(".results").offset().top
+					}, 2000);
+				}, 1000);
 			},
 			function(r) {
+				document.getElementById("CSSloaderOverlay").style.display = "none";
+				$scope.msg("No results found");
+				$scope.results = "";
 				// foursquare is not advaced as google in deciphering near, so this is needed for errors
 				// alert("Something went wrong, Please try a diffrent search or try another time.");
 			}
